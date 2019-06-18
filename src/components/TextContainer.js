200e620ec2cb897a2615b34c9ee3b2d2b2faf2b3
KhiavDim Lee <khiavdim@KhiavDims-MacBook-Pro.local>
2019-06-18 13:57:44 -0600

    Finished React-2-Mini

--- src/components/TextContainer.js
+++ src/components/TextContainer.js
@@ -6,6 +6,7 @@ export default class TextContainer extends Component {
     this.state = {
       text: '',
     };
+    this.updateText = this.updateText.bind(this)
   }
 
   updateText(e) {
@@ -17,8 +18,11 @@ export default class TextContainer extends Component {
   render() {
     return (
       <div className="textContainer">
-        <textarea
-          // style={  }
+        <textarea style = {{
+            fontFamily: this.props.fontFamily,
+            fontSize: this.props.fontSize,
+            color: this.props.fontColor
+            }}
           onChange={this.updateText}
           value={this.state.text}
           placeholder="Start typing your thoughts here!"
